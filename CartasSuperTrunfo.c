2d8c6a59637e8adca624acf8f72548843e2a026c
Elias01062006 <eliassouzaes7640995@gmail.com>
2025-03-14 14:59:45 +0000

    função para ler os dados da carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@
 typedef struct{
     char codigo[8];
     char cidade[100];
-    char estados[3];
+    char estado[3];
     int populacao;
     float area;
     float pib;
@@ -17,3 +17,37 @@ typedef struct{
     int c;
     while ((c = getchar()) != '\n'&& c!=EOF);
     }
+
+    //função para ler os dados de uma carta 
+    void lercarta (carta*carta,int numero){
+        printf("\n---Cadastro da carta %d ---\n", numero);
+
+        printf("Digite a sigla do estado(ex: SP, RJ):");
+        scanf("%2s",carta->estado);
+        limparbuffer();
+
+        printf("Digite o código da carta");
+        scanf("%7s",carta->codigo);
+        limparbuffer();
+
+        printf("digite o nome da cidade");
+        fgets(carta->cidade,
+            sizeof(carta->cidade),stdin);
+            carta -> cidade[strcspn(carta->cidade,"\n")]='\0'; // remove a quebra de linha
+
+        printf("Digite a população:");
+        scanf("%d",&carta->populacao);
+        limparbuffer();
+
+        printf("Digite a área (km):");
+        scanf("%f",&carta->area);
+        limparbuffer();
+
+        printf("Digite o pib:");
+        scanf("%f",&carta->pib);
+        limparbuffer();
+
+        printf("Digite a quantidades de pontos turisticos:");
+        scanf("%d",&carta->pontos_turisticos);
+        limparbuffer();
+        }
